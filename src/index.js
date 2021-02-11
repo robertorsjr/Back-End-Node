f355b474c9f3adab928e0784a6410069d6af8379
robertorsjr <beto.rodriguesds@gmail.com>
2021-02-11 19:53:22 -0300

    Api on air

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 const express = require('express');
 
-const app = express;
+const app = express();
+
+const projects = ['Desenvolvimento Web', 'Dsenvolvimento Mobile'];
+
+app.get('/',(request , response) => {
+  response.send(projects)
+});
+
+
+
+
+app.listen(3333);
