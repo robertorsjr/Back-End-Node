2543b8c725380ece6002d8c5c62cc13d38c25452
robertorsjr <beto.rodriguesds@gmail.com>
2021-02-12 00:37:13 -0300

    GET-POST-PUT-DELETE OK

--- src/index.js
+++ src/index.js
@@ -1,14 +1,56 @@
-const express = require('express');
+const { request } = require('express');
+const express = require('express'); 
+const {uuid} = require('uuidv4');
 
 const app = express();
 
-const projects = ['Desenvolvimento Web', 'Dsenvolvimento Mobile'];
+app.use(express.json());
 
-app.get('/',(request , response) => {
-  response.send(projects)
+const projects = [];
+
+app.get('/projects',(request , response) => {
+  const {title} = request.query;
+
+  const results = title
+  ? projects.filter(project => project.title.includes(title))
+  :projects;
+  return response.send(results)
+});
+
+app.post('/projects/',(request, response)=>{
+  const {title, owner} = request.body;
+  const project = {id:uuid(), title, owner};
+  projects.push(project);
+  return response.json(project);
+});
+
+app.put('/projects/:id', (request, response)=>{
+  const {id} = request.params;
+  const {title, owner} = request.body;
+  const projectIndex = projects.findIndex(project => project.id === id);
+  if(projectIndex < 0 ){
+    return response.status(400).json({ERROR:'Project not found.'});
+  }
+  const project = {
+    id,
+    title,
+    owner,
+  }
+  projects[projectIndex] = project;
+  return response.status(200).json(project);
+});
+
+app.delete('/projects/:id', (request, response)=>{
+  const {id} = request.params;
+  const projectIndex = projects.findIndex(project => project.id === id);
+  if(projectIndex < 0 ){
+    return response.status(400).json({ERROR:'Project not found.'});
+  }
+  projects.splice(projectIndex, 1);
+  return response.status(204).send();
 });
 
 
-
-
-app.listen(3333);
+app.listen(3333, ()=>{
+  console.log('Back-End Starts!')
+});
